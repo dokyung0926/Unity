d9c0185478716301929decec996bfd919b5cc4d1
Dokyeong <dlgnldhs0926@naver.com>
2021-12-21 22:01:01 +0900

    1221

---
+++ UnityLesson_CSharp_Basic/UnityLesson_CSharp_Array/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace UnityLesson_CSharp_Array
+{
+    class Program
+    {
+        // array
+        // 형태 : 자료형 [ ]
+        // 자료형이 정적으로 나열되어있는 형태 ( 한번 크기를 정해 놓으면 바꿀 수 없다 . )
+        static int[] arr_testInt = new int[5];
+        static float[] arr_testFloat1 = new float[3];
+        static float[] arr_testFloat2 = { 1.0f, 2.0f, 3.0f, 4.0f };
+        static string[] arr_testString = new string[3];
+        static void Main(string[] args)
+        {
+            arr_testInt[0] = 5;
+            arr_testInt[1] = 4;
+            arr_testInt[2] = 3;
+            arr_testInt[3] = 2;
+            arr_testInt[4] = 1;
+
+            Console.WriteLine(arr_testInt[0]);
+            Console.WriteLine(arr_testInt[1]);
+            Console.WriteLine(arr_testInt[2]);
+            Console.WriteLine(arr_testInt[3]);
+            Console.WriteLine(arr_testInt[4]);
+
+            arr_testFloat1[0] = 2.0f;
+            arr_testFloat1[1] = 4.0f;
+            arr_testFloat1[2] = 6.0f;
+
+            Console.WriteLine(arr_testFloat1[0]);
+            Console.WriteLine(arr_testFloat1[1]);
+            Console.WriteLine(arr_testFloat1[2]);
+
+            arr_testString[0] = "김아무개";
+            arr_testString[0] = "이아무개";
+            arr_testString[0] = "박아무개";
+
+            Console.WriteLine(arr_testString[0]);
+            Console.WriteLine(arr_testString[1]);
+            Console.WriteLine(arr_testString[2]);
+
+            string[] playerInfo = new string[999];
+
+            playerInfo[0] = "이름";
+            playerInfo[1] = "성별";
+        }
+    }
+    class testclass
+    {
+
+    }
+
+}
---
+++ UnityLesson_CSharp_Basic/UnityLesson_CSharp_ForLoopExample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+// orc 객체를 10마리 만들고 오크들의 인스턴스는 Orc타입 배열에 넣어준다.
+// 각 오크의 이름은 "오크 0", "오크 1" ...."오크 9"
+// 각 오크에세 isResting의 값을 true로 넣어준다
+// 각 오크가 쉬고있는지 확인해서 쉬고있다면 점프하도록 한다
+namespace UnityLesson_CSharp_ForLoopExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Orc[] arr_Orc = new Orc[10];
+            int length = arr_Orc.Length;
+            for (int i = 0; i < length; i++)
+            {
+                arr_Orc[i] = new Orc();
+                arr_Orc[i].name = $"오크{i}";
+            }
+            for (int i = 0; i < length; i++)
+            {
+                arr_Orc[i].isResting = GetRandomBool();
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                if (arr_Orc[i].isResting)
+                {
+                    arr_Orc[i].Jump();
+                }
+            }
+        }
+        static public bool GetRandomBool()
+        {
+            Random rand = new Random();
+            bool value = Convert.ToBoolean(rand.Next(0, 2));
+            return value;
+        }
+
+    }
+    class Orc
+    {
+        public string name;
+        public bool isResting;
+
+        public void Jump()
+        {
+            Console.WriteLine($"{name} (이)가 점프했다!");
+        }
+    }
+}
