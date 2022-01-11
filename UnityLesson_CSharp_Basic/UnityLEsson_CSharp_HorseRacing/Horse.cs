e42b766c11be769d9ecd99bfe788e474a8934c9c
Dokyeong <dlgnldhs0926@naver.com>
2022-01-11 22:04:24 +0900

    210111

---
+++ UnityLesson_CSharp_Basic/UnityLEsson_CSharp_HorseRacing/Horse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityLEsson_CSharp_HorseRacing
+{
+    class Horse
+    {
+        public string name;
+        public int distance;
+
+        public void Run(int moveDistance)
+        {
+            distance += moveDistance;
+        }
+    }
+}
