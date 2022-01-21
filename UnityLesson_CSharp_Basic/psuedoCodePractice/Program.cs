68cd21b73b841adecda5707e36f68517ad6ab425
Dokyeong <dlgnldhs0926@naver.com>
2022-01-21 22:05:02 +0900

    22y01m21d

---
+++ UnityLesson_CSharp_Basic/psuedoCodePractice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+// 세분화 예시 2 의 코드
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Sum1ToN(5));
+    }
+
+    static int Sum1ToN(int N)
+    {
+        int resultValue = 0;
+        for (int sumValue = 1; sumValue <= N; sumValue++)
+        {
+            resultValue += sumValue;
+        }
+        return resultValue;
+    }
+}
